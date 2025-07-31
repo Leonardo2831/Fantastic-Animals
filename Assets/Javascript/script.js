80706d98b7742e6a1094577723a626b6af4df2ad
Leonardo2831 <leoferraz1818@gmail.com>
2025-07-31 10:31:41 -0300

    new import function modal

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -3,9 +3,11 @@ import initShowSection from './components/showSection.js';
 import initTabNav from './components/tabNav.js';
 import initQuestion from './components/question.js';
 import initInfoContact from './components/infoContact.js';
+import initModalForm from './components/modalForm.js';
 
 initScrollSmooth();
 initShowSection();
 initTabNav();
 initQuestion();
 initInfoContact();
+initModalForm();

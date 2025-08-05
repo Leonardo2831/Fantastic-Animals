1f77a6e4ad6e7e8dd44d9a6dec3e92371e38cf98
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-05 10:59:42 -0300

    code to open and close dropdown

--- Assets/Javascript/components/dropDownNav.js
+++ Assets/Javascript/components/dropDownNav.js
@@ -5,11 +5,38 @@ export default function initDropDownNav(){
 
     const dropDowns = select.All('[data-dropdown]');
 
-    function handleClick(){
+    /**
+     * 
+     * @param {Element | null} element - elemento que foi clicado
+     * @param {Function} callback 
+     */
+    function clickOutside(element, events, callback){
+        const html = document.documentElement;
         
+        function handleOutside(event){
+            if(!element.contains(event.target)){
+                events.forEach((userEvent) => {
+                    html.removeEventListener(userEvent, handleOutside);
+                    callback();
+                });
+            }
+        }
+
+        events.forEach((userEvent) => {
+            html.addEventListener(userEvent, handleOutside);
+        });
+    }
+
+    function handleClick(){        
+        this.dataset.dropdown = this.dataset.dropdown = 'active';
+        clickOutside(this, ['touchstart', 'click'], () => {
+            this.dataset.dropdown = '';
+        });
     }
 
     dropDowns.forEach((dropDown) => {
-        dropDown.addEventListener('click', handleClick);
+        ['touchstart', 'click'].forEach((userEvent) => {
+            dropDown.addEventListener(userEvent, handleClick);
+        });
     });
 }

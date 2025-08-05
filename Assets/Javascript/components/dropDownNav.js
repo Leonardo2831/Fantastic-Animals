ed70070545b53182ec4c0b03d2f0cfaf21b89a29
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-05 13:09:29 -0300

    move click outside to new file

--- Assets/Javascript/components/dropDownNav.js
+++ Assets/Javascript/components/dropDownNav.js
@@ -1,31 +1,10 @@
 import { Select } from "./utilitaryFunctions.js";
+import clickOutside from "./clickOutside.js";
 
 export default function initDropDownNav(){
     const select = new Select();  
 
     const dropDowns = select.All('[data-dropdown]');
-
-    /**
-     * 
-     * @param {Element | null} element - elemento que foi clicado
-     * @param {Function} callback 
-     */
-    function clickOutside(element, events, callback){
-        const html = document.documentElement;
-        
-        function handleOutside(event){
-            if(!element.contains(event.target)){
-                events.forEach((userEvent) => {
-                    html.removeEventListener(userEvent, handleOutside);
-                    callback();
-                });
-            }
-        }
-
-        events.forEach((userEvent) => {
-            html.addEventListener(userEvent, handleOutside);
-        });
-    }
 
     function handleClick(){        
         this.dataset.dropdown = this.dataset.dropdown = 'active';

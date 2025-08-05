3eed52d4a6877a6ed9b7d444129b8abe2c259d5b
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-05 13:09:38 -0300

    new code to open and close menu mobile

--- Assets/Javascript/components/menu-mobile.js
+++ Assets/Javascript/components/menu-mobile.js
@@ -1,3 +1,25 @@
+import { Select } from './utilitaryFunctions.js';
+import clickOutside from './clickOutside.js';
+
 export default function initMenuMobile(){
-    
+    const select = new Select();
+
+    const buttonMobile = select.Single('[data-mobile="button"]');
+    const listMenu = select.Single('[data-mobile="list"]');
+    const events = ['click', 'touchstart'];
+
+    if(buttonMobile){
+        function openMenuMobile(){
+            const classActiveMenu = 'activeMobile';
+            buttonMobile.classList.toggle(classActiveMenu);
+
+            clickOutside(listMenu, events, () => {
+                buttonMobile.classList.remove(classActiveMenu);
+            });
+        }
+        
+        events.forEach((userEvent) => {
+            buttonMobile.addEventListener(userEvent, openMenuMobile);
+        });
+    }
 }

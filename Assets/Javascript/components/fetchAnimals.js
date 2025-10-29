4cea45038e3f565922e072671252dbefca8f370d
Leonardo2831 <leoferraz1818@gmail.com>
2025-10-28 22:34:22 -0300

    fetch animals

---
+++ Assets/Javascript/components/fetchAnimals.js
@@ -0,0 +1,39 @@
+import { Select } from './utilitarianFunctions.js';
+import initAnimationNumbers from './animationNumbers.js';
+
+export default function initFetchAnimals(){
+    const select = new Select();
+
+    async function fetchAnimals(url){
+        try {
+            const animalsResponse = await (await fetch(url)).json();
+            const contentAnimals = select.Single('[data-animals="number"]');
+            
+            animalsResponse.animals.forEach((animal) => {
+                const divAnimalCompleted = createAnimal(animal);
+                contentAnimals.appendChild(divAnimalCompleted);
+            });
+            
+            initAnimationNumbers();
+        } catch(error){
+            console.log(Error(error));
+        }
+    }
+
+    function createAnimal(animal){
+        const divAnimal = document.createElement('div');
+        const htmlAnimal = `
+            <h3>${animal.specie}</h3>
+            <span data-number>${animal.total}</span>
+        `;
+
+        const classDivStyle = 'number-animal'
+
+        divAnimal.classList.add(classDivStyle);
+        divAnimal.innerHTML = htmlAnimal;
+
+        return divAnimal;
+    }
+
+    fetchAnimals('Assets/data/animals.json');
+}

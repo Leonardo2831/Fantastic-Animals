6e87eab796e771acf1cbdaad67fd07a8ff5e4765
Leonardo2831 <leoferraz1818@gmail.com>
2025-10-28 22:34:29 -0300

    fetch bitcoin

---
+++ Assets/Javascript/components/bitcoin.js
@@ -0,0 +1,16 @@
+import { Select } from './utilitarianFunctions.js';
+
+export default function initBitCoinFetch(){
+    const select = new Select();
+
+    fetch('https://blockchain.info/ticker')
+        .then((response) => response.json())
+        .then((bitcoin) => {
+            const contentValueBitcoin = select.Single('data-value="bitcoin"');
+
+            contentValueBitcoin.textContent = (1000 / bitcoin.BRL.sell).toFixed(4);
+        })
+        .catch((error) => {
+            console.log(Error(error));
+        });
+}

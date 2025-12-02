57095e8275c1b1d54e73943383237a64079325e1
Leonardo2831 <leoferraz1818@gmail.com>
2025-12-01 21:05:19 -0300

    new config devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
     entry: './Assets/Javascript/script.js',
@@ -6,16 +6,23 @@ module.exports = {
         path: path.resolve(__dirname, './Assets/Javascript'),
         filename: 'main.js'
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+        port: 3000,
+        open: true,
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
+                        presets: ["@babel/preset-env"],
+                        plugins: ["@babel/plugin-transform-runtime"],
                     },
                 },
             },

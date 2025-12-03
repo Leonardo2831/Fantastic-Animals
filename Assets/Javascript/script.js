27ea3df414ea1a10adb69b80aea3753e57fb835a
Leonardo2831 <leoferraz1818@gmail.com>
2025-12-03 20:29:08 -0300

    init new tabNav

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,7 +1,8 @@
 import ScrollSmooth from './components/scrollSmooth.js';
 import Questions from './components/question.js';
+import TabNav from './components/tabNav.js';
+
 import initShowSection from './components/showSection.js';
-import initTabNav from './components/tabNav.js';
 import initInfoOperation from './components/infoContact.js';
 import initModalForm from './components/modalForm.js';
 import initToolTip from './components/tooltip.js';
@@ -10,6 +11,7 @@ import initMenuMobile from './components/menu-mobile.js';
 import initFetchAnimals from './components/fetchAnimals.js';
 import initBitCoinFetch from './components/bitcoin.js';
 
+// scroll
 const optionsScroll = {
     behavior: "smooth",
     block: "start",
@@ -17,11 +19,15 @@ const optionsScroll = {
 const scrollSmooth = new ScrollSmooth("[data-scroll='behavior'] a[href^='#']", optionsScroll);
 scrollSmooth.init();
 
+// questions
 const questions = new Questions("[data-faq] dl dt", "openQuestion");
 questions.init();
 
+// tabNav
+const tabNav = new TabNav("[data-animals='tab'] picture img", '[data-animals="tab"] article', "activeAnimal");
+tabNav.init();
+
 initShowSection();
-initTabNav();
 initInfoOperation();
 initModalForm();
 initToolTip();

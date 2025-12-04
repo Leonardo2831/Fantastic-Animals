447d56da8554d3da2cac283a7d3a5a35f0e37dae
Leonardo2831 <leoferraz1818@gmail.com>
2025-12-03 21:57:26 -0300

    init tool tip

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,22 +1,25 @@
-import ScrollSmooth from './components/scrollSmooth.js';
-import Questions from './components/question.js';
-import TabNav from './components/tabNav.js';
-import ModalForm from './components/modalForm.js';
-import ToolTip from './components/tooltip.js';
+import ScrollSmooth from "./components/scrollSmooth.js";
+import Questions from "./components/question.js";
+import TabNav from "./components/tabNav.js";
+import ModalForm from "./components/modalForm.js";
+import ToolTip from "./components/tooltip.js";
 
-import initShowSection from './components/showSection.js';
-import initInfoOperation from './components/infoContact.js';
-import initDropDownNav from './components/dropDownNav.js';
-import initMenuMobile from './components/menu-mobile.js';
-import initFetchAnimals from './components/fetchAnimals.js';
-import initBitCoinFetch from './components/bitcoin.js';
+import initShowSection from "./components/showSection.js";
+import initInfoOperation from "./components/infoContact.js";
+import initDropDownNav from "./components/dropDownNav.js";
+import initMenuMobile from "./components/menu-mobile.js";
+import initFetchAnimals from "./components/fetchAnimals.js";
+import initBitCoinFetch from "./components/bitcoin.js";
 
 // scroll
 const optionsScroll = {
     behavior: "smooth",
     block: "start",
 };
-const scrollSmooth = new ScrollSmooth("[data-scroll='behavior'] a[href^='#']", optionsScroll);
+const scrollSmooth = new ScrollSmooth(
+    "[data-scroll='behavior'] a[href^='#']",
+    optionsScroll
+);
 scrollSmooth.init();
 
 // questions
@@ -24,11 +27,15 @@ const questions = new Questions("[data-faq] dl dt", "openQuestion");
 questions.init();
 
 // tabNav
-const tabNav = new TabNav("[data-animals='tab'] picture img", '[data-animals="tab"] article', "activeAnimal");
+const tabNav = new TabNav(
+    "[data-animals='tab'] picture img",
+    '[data-animals="tab"] article',
+    "activeAnimal"
+);
 tabNav.init();
 
 // modal form
-const modalForm = new ModalForm('[data-modal]', '[data-button-modal]');
+const modalForm = new ModalForm("[data-modal]", "[data-button-modal]");
 modalForm.init();
 
 // toolTip
@@ -37,8 +44,6 @@ toolTip.init();
 
 initShowSection();
 initInfoOperation();
-ModalForm();
-initToolTip();
 initDropDownNav();
 initMenuMobile();
 initFetchAnimals();
